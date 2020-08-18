308adb8f8e3bb0beb1f8421996fb76300b9b85f1
SheillyR <sheillyrolo@gmail.com>
2020-08-18 17:38:34 -0500

    Show Cards v.0.0

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
+// import data from './data/pokemon/pokemon.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const charactersCards = (characters) => {
+    return `
+    <div class = 'cards'>
+        <img class = 'photos' src = "${characters.image}">
+        <h2 class = 'names'>${characters.name}</h2>
+        <p><strong>Specie:</strong> ${characters.species}</p>
+    </div>
+    `
+}
+
+document.getElementById('container').innerHTML = `
+${data.results.map(charactersCards).join('')}
+`
