17216aecf8cc6fb33421b87613b22bca5bba2a70
SheillyR <sheillyrolo@gmail.com>
2020-09-04 22:53:10 -0500

    Implemented test to filterData

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,29 +1,4 @@
-//import { example, anotherExample } from '../src/data.js';
-
-/*
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
-*/
-
-// Importamos la función `filterDataByPrperty`
+// Import function `filterData`
 
 import {filterData} from '../src/data.js';
 
